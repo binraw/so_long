863c98519dc00bd841e3bf9b86cbd65a1f1818fb
Robin Truvelot <76820063+binraw@users.noreply.github.com>
2024-02-17 11:06:47 +0100

    Update end_game.c

--- end_game.c
+++ end_game.c
@@ -2,23 +2,23 @@
 
 void    destroy_map(t_game *data)
 {
-    while (game->count_line > 0)
+    while (data->numb_line > 0)
 	{
-		free(game->map[data->count_line]);
-		game->count_line_maap--;
+		free(data->map[data->numb_line]);
+		data->numb_line--;
 	}
-	free(game->map[data->count_line]);
+	free(data->map[data->numb_line]);
 	return ;
 }
 
 void    destroy_img(t_game *data)
 {
-    mlx_destroy_image(data->mlx_ptr, data->img->img_wall);
-	mlx_destroy_image(data->mlx_ptr, data->img->img_player);
-	mlx_destroy_image(data->mlx_ptr, data->img->img_background);
-	mlx_destroy_image(data->mlx_ptr, data->img->img_bag);
-	mlx_destroy_image(data->mlx_ptr, data->img->img_door);
-	free_map(game);
-	free(game->mlx_pointer);
+    mlx_destroy_image(data->mlx_ptr, data->img.img_wall);
+	mlx_destroy_image(data->mlx_ptr, data->img.img_player);
+	mlx_destroy_image(data->mlx_ptr, data->img.img_background);
+	mlx_destroy_image(data->mlx_ptr, data->img.img_bag);
+	mlx_destroy_image(data->mlx_ptr, data->img.img_door);
+	destroy_map(data);
+	free(data->mlx_ptr);
 	return ;
 }
