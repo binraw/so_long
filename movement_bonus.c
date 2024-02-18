bc06615de21a0e770a43e1113de8a814c1bd12c0
Robin Truvelot <76820063+binraw@users.noreply.github.com>
2024-02-18 14:49:11 +0100

    Update movement_bonus.c

--- movement_bonus.c
+++ movement_bonus.c
@@ -126,137 +126,119 @@ int    key_hook(int keycode, t_game *data)
 {
 
     if (keycode == XK_w || keycode == XK_Up)
-    {
 		key_up(data);
-    }
      else if (keycode == XK_s || keycode == XK_Down)
-    {
 		key_down(data);
 
-    }
      else if (keycode == XK_a || keycode == XK_Left)
-    {
 		key_left(data);
- 
-        
-    }
      else if (keycode == XK_d || keycode == XK_Right)
-    {
 		key_right(data);
- 
-    }
     else if (keycode == XK_Escape)
-    {
         exit(EXIT_SUCCESS);
-    }
     return (0);
 }
 
 void	reset_count_move(t_game *data)
 {
 	data->count_left = 0;
-    data->reta_right = 0;
-    data->count_face = 0;
-    data->count_back = 0;
+	data->reta_right = 0;
+	data->count_face = 0;
+	data->count_back = 0;
 }
 void    enemy_move(t_game *data, int new_y, int new_x)
 {
-    int	last_x;
+	int	last_x;
 	int	last_y;
-    int collect;
-    
-    last_x = data->pos_enemy_x;
-    last_y = data->pos_enemy_y;
-    collect = count_elem(data, 'C');
-    if (data->map[new_y][new_x] == '0')
-    {
-        data->map[last_y][last_x] = '0';
-        data->map[new_y][new_x] = 'A';
-        data->pos_enemy_x = new_x;
-        data->pos_enemy_y = new_y;
-    }
-    else if (data->map[new_y][new_x] == 'P')
-    {
-        ft_printf("You LOOSE !");
+	int collect;
+
+	last_x = data->pos_enemy_x;
+	last_y = data->pos_enemy_y;
+	collect = count_elem(data, 'C');
+	if (data->map[new_y][new_x] == '0')
+	{
+		data->map[last_y][last_x] = '0';
+        	data->map[new_y][new_x] = 'A';
+        	data->pos_enemy_x = new_x;
+        	data->pos_enemy_y = new_y;
+	}
+	else if (data->map[new_y][new_x] == 'P')
+	{
+		ft_printf("You LOOSE !");
 		destroy_img(data);
-        exit(EXIT_SUCCESS);
-    }
+		exit(EXIT_SUCCESS);
+	}
     
     // init_maap(data);
 }
 
-
 void move(t_game *data, int new_y, int new_x)
 {
-    int	last_x;
+	int	last_x;
 	int	last_y;
-    
-    last_x = data->pos_x;
-    last_y = data->pos_y;
-    data->map[last_y][last_x] = '0';
-    data->map[new_y][new_x] = 'P';
-    data->pos_x = new_x;
-    data->pos_y = new_y;
-}
 
+	last_x = data->pos_x;
+	last_y = data->pos_y;
+	data->map[last_y][last_x] = '0';
+	data->map[new_y][new_x] = 'P';
+	data->pos_x = new_x;
+	data->pos_y = new_y;
+}
 
 void    player_move(t_game *data, int new_y, int new_x)
 {
-    int collect;
-    
-    collect = count_elem(data, 'C');
-    if (data->map[new_y][new_x] == '0')
-    {
-        move(data, new_y, new_x);
-    }
-    else if (data->map[new_y][new_x] == 'C')
-    {
-        move(data, new_y, new_x);
-        collect--;
-    }
+	int collect;
+
+	collect = count_elem(data, 'C');
+	if (data->map[new_y][new_x] == '0')
+	{
+		move(data, new_y, new_x);
+	}
+	else if (data->map[new_y][new_x] == 'C')
+	{
+		move(data, new_y, new_x);
+		collect--;
+	}
         else if (data->map[new_y][new_x] == 'A')
-    {
-        ft_printf("You LOOSE !");
+	{
+        	ft_printf("You LOOSE !");
 		destroy_img(data);
-        exit(EXIT_SUCCESS);
-    }
-    else if (data->map[new_y][new_x] == 'E' && collect == 0)
-    {
+        	exit(EXIT_SUCCESS);
+	}
+	else if (data->map[new_y][new_x] == 'E' && collect == 0)
+	{
 		destroy_img(data);
-        exit(EXIT_SUCCESS);
-    }
-    
+		exit(EXIT_SUCCESS);
+	}
 	reset_count_move(data);
-    init_maap(data);
+	init_maap(data);
 }
 
 void	update_animation(t_game *data, char *player_path, char *bag_path, char *wall_path)
 {
 	updapte_img_player(data,  player_path);
-    updapte_img_bag(data, bag_path);
-    updapte_img_wall(data,  wall_path);
+	updapte_img_bag(data, bag_path);
+	updapte_img_wall(data,  wall_path);
 }
 
 
 void    control_move(t_game *data, int new_y, int new_x)
 {
-    int	last_x;
+	int	last_x;
 	int	last_y;
     
-    last_x = data->pos_x;
-    last_y = data->pos_y;
-    if (last_x < new_x)
+	last_x = data->pos_x;
+	last_y = data->pos_y;
+	if (last_x < new_x)
 		update_animation(data, "./assets/right_run.xpm", "./assets/caisse4.xpm", "./assets/fire3.xpm" );
 
-    if (last_x > new_x)
+	if (last_x > new_x)
 		update_animation(data, "./assets/left_run.xpm", "./assets/caisse2.xpm", "./assets/fire2.xpm" );
 
-      if (last_y > new_y)
+	if (last_y > new_y)
 		update_animation(data, "./assets/back_run.xpm", "./assets/caisse4.xpm", "./assets/fire2.xpm" );
-
-       if (last_y < new_y)
+	if (last_y < new_y)
 		update_animation(data,"./assets/face_run.xpm", "./assets/caisse2.xpm" , "./assets/fire3.xpm");
- 
 }
 
 void    screen_number_move(t_game *data)
