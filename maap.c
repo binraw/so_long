9fdef8a220d3ddea33586deab018dfe69710b95f
Robin Truvelot <76820063+binraw@users.noreply.github.com>
2024-02-18 14:39:20 +0100

    Update maap.c

--- maap.c
+++ maap.c
@@ -17,13 +17,11 @@ void init_maap(t_game *data)
 {
 	int		y;
 	int		i;
-	
 
 	y = 0;
 	i = 0;
 	if (!data->map)
 		return ;
-
 	while (data->map[y])
 	{
 		i = 0;
@@ -34,29 +32,18 @@ void init_maap(t_game *data)
 		}
 		y++;
 	}
-	
 }
-
-
 
 void	screen_model(char c, t_game *data, int i, int y)
 {
 	if (c == '1')
-	{
 		print_img(data, data->img.img_wall, i, y);
-	}
 	if (c == '0')
-	{
 		print_img(data, data->img.img_background, i, y);
-	}
 	if (c == 'C')
-	{
 		print_img(data, data->img.img_bag, i, y);
-	}
 	if (c == 'E')
-	{
 		print_img(data, data->img.img_door, i, y);
-	}
 	if (c == 'P')
 	{
 		print_img(data, data->img.img_player, i, y);
@@ -94,10 +81,8 @@ int count_line_maap(char *file)
 {
 	int		count;
 	int		fd;
-	int 	reader;
 	char	*lign;
 	
-	reader = 0;
 	fd = open(file, O_RDONLY);
 	count = -1;
 	lign = "lign";
@@ -120,3 +105,4 @@ void		alloc_lign(char *file, t_game *data)
 	line_count = count_line_maap(file);
 	data->map = malloc(sizeof(char *) * line_count + 1);
 }
+
