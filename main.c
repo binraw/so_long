7fd11e21d03bf852004d7cac0195610fe50d9da9
Robin Truvelot <76820063+binraw@users.noreply.github.com>
2024-02-18 14:41:55 +0100

    Update main.c

--- main.c
+++ main.c
@@ -9,20 +9,9 @@
 /*   Updated: 2024/02/16 16:02:20 by rtruvelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-
-
 #include "so_long.h"
 #include <stdlib.h>
 #include <stdio.h>
-
-
-
-
-
-
-
-
 
 
 int	main(int argc, char **argv)
@@ -41,21 +30,14 @@ int	main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 	vars.mlx_win = mlx_new_window(vars.mlx_ptr, (ft_strlen_get(vars.map[0]) - 1) * 50, (count_line_maap(argv[1])) * 50, "Hello world!");
-
 	vars.count_left = 0;
 	vars.reta_right = 0;
 	vars.count_back = 0;
 	vars.count_face = 0;
 	vars.move = 0;
-	
-
 	set_img(&vars);
-	
 	init_maap(&vars);
 	mlx_key_hook(vars.mlx_win, key_hook, &vars);
-
-
-	
 	mlx_loop(vars.mlx_ptr);
 	return (0);
 }
