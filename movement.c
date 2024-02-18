65795ecc53bbddd12cf44ed917b2b22eaf2404e4
Robin Truvelot <76820063+binraw@users.noreply.github.com>
2024-02-18 14:27:14 +0100

    Update movement.c

--- movement.c
+++ movement.c
@@ -154,35 +154,46 @@ void	reset_count_move(t_game *data)
 }
 
 
-void    player_move(t_game *data, int new_y, int new_x)
+void move(t_game *data, int new_y, int new_x)
 {
     int	last_x;
 	int	last_y;
-    int collect;
     
     last_x = data->pos_x;
     last_y = data->pos_y;
+    data->map[last_y][last_x] = '0';
+    data->map[new_y][new_x] = 'P';
+    data->pos_x = new_x;
+    data->pos_y = new_y;
+}
+
+
+void    player_move(t_game *data, int new_y, int new_x)
+{
+    int collect;
+    
     collect = count_elem(data, 'C');
     if (data->map[new_y][new_x] == '0')
     {
-        data->map[last_y][last_x] = '0';
-        data->map[new_y][new_x] = 'P';
-        data->pos_x = new_x;
-        data->pos_y = new_y;
+        move(data, new_y, new_x);
     }
     else if (data->map[new_y][new_x] == 'C')
     {
-        data->map[last_y][last_x] = '0';
-        data->map[new_y][new_x] = 'P';
-        data->pos_x = new_x;
-        data->pos_y = new_y;
+        move(data, new_y, new_x);
         collect--;
+    }
+        else if (data->map[new_y][new_x] == 'A')
+    {
+        ft_printf("You LOOSE !");
+		destroy_img(data);
+        exit(EXIT_SUCCESS);
     }
     else if (data->map[new_y][new_x] == 'E' && collect == 0)
     {
 		destroy_img(data);
         exit(EXIT_SUCCESS);
     }
+    
 	reset_count_move(data);
     init_maap(data);
 }
